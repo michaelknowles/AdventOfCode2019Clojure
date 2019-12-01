fc6bca5ac5151b62f8d77b66359bed5677e79c25
Mike Knowles <mikewknowles@outlook.com>
2019-12-01 13:43:48 -0800

    Day 1 Part 1 Solved

---
+++ src/advent_2019/day1.clj
@@ -0,0 +1,15 @@
+(ns advent-2019.day1
+  (:require [advent.util :as util]))
+
+(def input-part1 (util/read-file "2019-day1.txt"))
+
+(defn find-fuel
+  [mass]
+  (- (Math/floor
+      (/ (Integer/parseInt mass) 3))
+     2))
+
+(defn day1
+  "Answer: 3297896"
+  [input]
+  (reduce + (map find-fuel input)))
