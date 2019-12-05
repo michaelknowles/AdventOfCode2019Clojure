89c979c17afc06a46051da0176dbdd140bcd7235
Mike Knowles <mikewknowles@outlook.com>
2019-12-05 08:35:57 -0800

    Day 1 solved

--- src/advent_2019/day1.clj
+++ src/advent_2019/day1.clj
@@ -1,13 +1,36 @@
 (ns advent-2019.day1
   (:require [advent.util :as util]))
 
-(def input-part1 (util/read-file "2019-day1.txt"))
+(def input-day1
+  (map clojure.edn/read-string (util/read-file "2019-day1.txt")))
 
-(defn find-fuel
+(defn mass->fuel
+  "The fuel is the mass divided by 3, rounded down, and subtracted by 2"
   [mass]
-  (- (quot (Integer/parseInt mass) 3) 2))
+  (- (quot mass 3) 2))
 
-(defn day1
+(defn part1
   "Answer: 3297896"
   [input]
-  (reduce + (map find-fuel input)))
+  (reduce + (map mass->fuel input)))
+
+(defn iter-mass->fuel
+  "The fuel is the mass divided by 3, rounded down, and subtracted by 2.
+   The fuel itself requires fuel so find the fuel required for that as well.
+   The fuel cannot be negative."
+  [mass]
+  (->> (iterate mass->fuel mass)
+       (take-while pos?)
+       (rest)
+       (reduce +)))
+
+(defn part2
+  "Answer: 4943969"
+  [input]
+  (reduce + (map iter-mass->fuel input)))
+
+(defn day1
+  [input]
+  (assoc {}
+         :part1 (part1 input-day1)
+         :part2 (part2 input-day1)))
